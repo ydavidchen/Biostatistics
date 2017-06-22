41e58e9cf8ba48cb06bc95cce8152c4fa50e8ab0
David Chen <youdinghuan.chen.gr@dartmouth.edu>
2017-06-22 11:56:51 -0400

    Survival analysis criteria updated

--- 06_first_fall_survival.R
+++ 06_first_fall_survival.R
@@ -3,7 +3,11 @@
 # David Chen
 # Start date: 05/28/2017
 # Notes:
+# 1. Disregard everything after the first fall or first no-show
+# 2. However, for subjects with both event (first fall) & no-show: prioritize the event
 #############################################################################################
+
+rm(list=ls())
 
 library(dplyr)
 library(gdata)
@@ -30,95 +34,136 @@ RACE_data_dict$Code[RACE_data_dict$Variable.Group.Name=="Demographics"]
 RACE_data_dict$Code[RACE_data_dict$Variable.Group.Name=="Follow-up:  Falls"]
 RACE_data_dict$Code[RACE_data_dict$Variable.Group.Name=="Baseline Assessment"]
 
+
+RACE_data.screen <- RACE_data[RACE_data$IntervalName=="Screening", ]
+RACE_data.rest <- RACE_data[RACE_data$IntervalName!="Screening", ]
+mean(unique(RACE_data.rest$ReferenceID) %in% RACE_data.screen$ReferenceID)
+
 #------------------------------------Extract binary fall/no fall for each time point------------------------------------
 ## Survival analysis with censoring:
 RACE_data.surv <- RACE_data[ , c("ReferenceID", "TreatmentCode","IntervalName","fallen_past_3months")]
 RACE_data.surv <- RACE_data.surv[! RACE_data.surv$IntervalName %in% c("End of Treatment","Repeat Treatment"), ]
 RACE_data.surv$fallen_past_3months[is.na(RACE_data.surv$fallen_past_3months)] <- 0
-
-## Code in time point in numeric format for sorting:
-RACE_data.surv$timepoint <- NA;
-RACE_data.surv$timepoint[RACE_data.surv$IntervalName=="Screening"         ] <-  0;
-RACE_data.surv$timepoint[RACE_data.surv$IntervalName=="3-Month Follow-up" ] <-  3;
-RACE_data.surv$timepoint[RACE_data.surv$IntervalName=="6-Month Follow-up" ] <-  6;
-RACE_data.surv$timepoint[RACE_data.surv$IntervalName=="9-Month Follow-up" ] <-  9;
-RACE_data.surv$timepoint[RACE_data.surv$IntervalName=="Annual Follow-up"  ] <- 12;
-RACE_data.surv$timepoint[RACE_data.surv$IntervalName=="15-Month Follow-up"] <- 15;
-RACE_data.surv$timepoint[RACE_data.surv$IntervalName=="18-Month Follow-up"] <- 18;
-RACE_data.surv$timepoint[RACE_data.surv$IntervalName=="21-Month Follow-up"] <- 21;
-RACE_data.surv$timepoint[RACE_data.surv$IntervalName=="2-Year Follow-up"  ] <- 24
-
-RACE_data.surv <- RACE_data.surv[with(RACE_data.surv, order(ReferenceID, timepoint)), ] # Sort by vector name [ReferenceID] then [timepoint]
-RACE_data.surv <- droplevels(RACE_data.surv)
-
+patients <- unique(RACE_data.surv$ReferenceID); #all unique patients
 activeStepIDs <- unique(RACE_data.surv$ReferenceID[RACE_data.surv$TreatmentCode=="ACTIVESTEP"]) #for query later
 
-## For rows with 1 for a given patient, drop rows after the first event:
-last_timePt <- "(no event)"; #for people never fell
-patients <- unique(RACE_data.surv$ReferenceID);
+## Code in time point in numeric format for sorting:
+RACE_data.surv$IntervalName.num <- NA;
+RACE_data.surv$IntervalName.num[RACE_data.surv$IntervalName=="Screening"         ] <-  0;
+RACE_data.surv$IntervalName.num[RACE_data.surv$IntervalName=="3-Month Follow-up" ] <-  3;
+RACE_data.surv$IntervalName.num[RACE_data.surv$IntervalName=="6-Month Follow-up" ] <-  6;
+RACE_data.surv$IntervalName.num[RACE_data.surv$IntervalName=="9-Month Follow-up" ] <-  9;
+RACE_data.surv$IntervalName.num[RACE_data.surv$IntervalName=="Annual Follow-up"  ] <- 12;
+RACE_data.surv$IntervalName.num[RACE_data.surv$IntervalName=="15-Month Follow-up"] <- 15;
+RACE_data.surv$IntervalName.num[RACE_data.surv$IntervalName=="18-Month Follow-up"] <- 18;
+RACE_data.surv$IntervalName.num[RACE_data.surv$IntervalName=="21-Month Follow-up"] <- 21;
+RACE_data.surv$IntervalName.num[RACE_data.surv$IntervalName=="2-Year Follow-up"  ] <- 24;
+RACE_data.surv$IntervalName.num <- as.numeric(RACE_data.surv$IntervalName.num);
+
+RACE_data.surv <- RACE_data.surv[with(RACE_data.surv, order(ReferenceID, IntervalName.num)), ]; # Sort by vector name [ReferenceID] then [IntervalName.num]
+RACE_data.surv <- droplevels(RACE_data.surv);
+
+RACE_data.screen <- RACE_data.surv[RACE_data.surv$IntervalName=="Screening", ]
+RACE_data.rest <- RACE_data.surv[RACE_data.surv$IntervalName != "Screening", ]
+mean(RACE_data.rest$ReferenceID %in% RACE_data.screen$ReferenceID)
+
+## Add in censoring time:
+RACE_data.surv$CensoringTime <- NA; 
+for(id in patients){
+  df <- RACE_data.surv[RACE_data.surv$ReferenceID==id, ];
+  RACE_data.surv$CensoringTime[RACE_data.surv$ReferenceID==id] <- df$IntervalName.num[nrow(df)] #last available record
+}
+class(RACE_data.surv$CensoringTime) #"numeric" required
+
+## Data frame for survival analysis:
+last_timePt <- Inf; #identifier (of type numeric) for people never fell
 first_fall_rec <- NULL;
 for(id in patients){
   ## Select a subject's records:
-  subj_indices <- which(RACE_data.surv$ReferenceID %in% id)
-  DFi <-  RACE_data.surv[subj_indices, ]
+  subj_indices <- which(RACE_data.surv$ReferenceID %in% id);
+  DFi <-  RACE_data.surv[subj_indices, ];
   
   ## Conditional update: if a patient ever fell...
+  ## The first instance of fall will be the "event encounter"
   if(c(1) %in% DFi$fallen_past_3months){
-    IntervalName <- DFi$IntervalName[DFi$fallen_past_3months==1][1]
+    EventTime <- as.numeric(DFi$IntervalName.num[DFi$fallen_past_3months == 1][1]);
   }else{
-    IntervalName <- last_timePt
+    EventTime <- last_timePt;
   }
   
+  ## Conditional update: if a patient ever failed to show up:
+  ## The first instance of missing will be censored time
+  CensoringTime <- as.numeric(unique(DFi$CensoringTime)); # should only have 1 censoring time
+  
   ## Update results matrix:
-  first_fall_rec <- rbind(
+  first_fall_rec <- rbind.data.frame(
     first_fall_rec, 
-    cbind(id, IntervalName)
-  )
-}
+    cbind.data.frame(id, EventTime, CensoringTime)
+  );
+};
 
-first_fall_rec <- as.data.frame(first_fall_rec);
-
-first_fall_rec$timepoint <- NA;
-first_fall_rec$timepoint[first_fall_rec$IntervalName=="Screening"         ] <-  0;
-first_fall_rec$timepoint[first_fall_rec$IntervalName=="3-Month Follow-up" ] <-  3;
-first_fall_rec$timepoint[first_fall_rec$IntervalName=="6-Month Follow-up" ] <-  6;
-first_fall_rec$timepoint[first_fall_rec$IntervalName=="9-Month Follow-up" ] <-  9;
-first_fall_rec$timepoint[first_fall_rec$IntervalName=="Annual Follow-up"  ] <- 12;
-first_fall_rec$timepoint[first_fall_rec$IntervalName=="15-Month Follow-up"] <- 15;
-first_fall_rec$timepoint[first_fall_rec$IntervalName=="18-Month Follow-up"] <- 18;
-first_fall_rec$timepoint[first_fall_rec$IntervalName=="21-Month Follow-up"] <- 21;
-first_fall_rec$timepoint[first_fall_rec$IntervalName=="2-Year Follow-up"  ] <- 24;
-first_fall_rec$timepoint[first_fall_rec$IntervalName=="(no event)"        ] <- 24 #or Inf?
-
-## Code in events:
-first_fall_rec$event <- ifelse(
-  test = first_fall_rec$IntervalName == "(no event)", 
-  yes  = FALSE,
-  no   = TRUE
-)
+## Add in group:
 first_fall_rec$TreatmentCode <- ifelse(
   test = first_fall_rec$id %in% activeStepIDs,
   yes  = "ACTIVESTEP",
   no   = "STANDARD"
-)
-colnames(first_fall_rec)
+); 
+
+## Define events by comparing Censoring vs. Event time
+first_fall_rec$event <- NA;
+for(i in 1:nrow(first_fall_rec)){
+  ## First code in events solely based on EventTime
+  if(first_fall_rec$EventTime[i] == Inf){
+    first_fall_rec$event[i] <- 0; #"alive"
+  } else {
+    first_fall_rec$event[i] <- 1; #"dead"
+  };
+  
+  ## Second, if a patient had both 1+ fall or no-show:
+  ## 1) Whether censored or not
+  ##    24mo is the "administrative censoring" and does not count!
+  ##     0mo is the exception!
+  ## 2) If both an Event & censoring occurred during the study period, Event should take priority over censoring
+  ## We are interested in those cases that are NOT administratively censored
+  if( #If during this study period an event ever occurred AND censoring ever occurred
+    ( ! first_fall_rec$CensoringTime[i] %in%  c(0,24) ) & 
+    ( first_fall_rec$EventTime != Inf                 )
+  ){
+    first_fall_rec$event[i] <- 1; #"dead"
+  } else if ( #If censoring occurred, but no fall, then considered alive
+    ( ! first_fall_rec$CensoringTime[i] %in%  c(0,24) ) & 
+    ( first_fall_rec$EventTime == Inf                 )
+  ){
+    first_fall_rec$event[i] <- 0; #"dead"
+  };
+};
+colnames(first_fall_rec);
+# first_fall_rec$start <- 0; # needed for interval censoring
+class(first_fall_rec$start);
+class((first_fall_rec$EventTime));
+class((first_fall_rec$event))
 
 ## Kaplan-Meier Curve:
-with(first_fall_rec, Surv(timepoint, event))
-mySurv <- Surv(first_fall_rec$timepoint, event=first_fall_rec$event)
-myFit  <- survfit(mySurv ~ TreatmentCode, data=first_fall_rec)
+## Use default settings rather than interval
+rm(EventTime) #avoid confusion
+first_fall_rec$EventTime[first_fall_rec$EventTime==Inf] <- 24; #restore to enable plotting
+mySurv <- Surv(first_fall_rec$EventTime, event=first_fall_rec$event);
+mySurv
+myFit  <- survfit(mySurv ~ TreatmentCode, data=first_fall_rec);
 summary(myFit)
 survdiff(mySurv ~ TreatmentCode, data=first_fall_rec) #log-rank test
-# autoplot(myFit, conf.int=FALSE) + 
-#   labs(x="Time in months", y="Survival probability", title="Comparison of time-to-event") +
-#   scale_color_brewer(palette="Set1") +
-#   theme_bw()
+
+autoplot(myFit, conf.int=FALSE, censor=FALSE) +
+  labs(x="Time in months", y="Survival probability", title="Comparison of time-to-event") +
+  scale_color_brewer(palette="Set1") +
+  theme_bw()
+
 ggsurvplot(
   myFit, 
   data = first_fall_rec, 
-  risk.table = TRUE,
-  palette = "Set1", 
-  pval = TRUE, 
-  xlab = "Time in months"
+  risk.table = TRUE, #show risk table
+  censor = FALSE, #don't draw bars
+  pval = TRUE,  #show log-rank test P-value
+  xlab = "Time in months",
+  palette = "Set1"
 )
-
